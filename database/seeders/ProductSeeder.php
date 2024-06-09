997b98f7cc325d90d08a8eaba6c96564f6f047c5
mohamed awad <mohamed.awad.elgammal@gmail.com>
2024-06-09 21:54:57 +0200

    add product seeders and product controller

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Ramsey\Uuid\Type\Integer;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('products')->insert([
+            'name' => Str::random(20),
+            'description' => Str::random(80),
+            'price' => rand(100,1000)
+        ]);
+     }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'user'],function(){
+    Route::get('products',[ProductController::class,'index']);
+});
+
+Route::group(['prefix' => 'admin'],function(){
+    Route::resource('products',ProductController::class);
 });
